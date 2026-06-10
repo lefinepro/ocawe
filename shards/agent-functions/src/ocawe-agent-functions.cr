a4972531fba1deaa931f1d62d2ca9471172f7f69
kogeletey <kg@re128.org>
2026-06-10 22:02:47 +0300

    fix: relicense only 0BSD and remove shards functions

--- shards/agent-functions/src/ocawe-agent-functions.cr
+++
@@ -1,9 +0,0 @@
-require "http/client"
-require "json"
-
-require "./ocawe/agent_function_handlers/helpers"
-require "./ocawe/agent_function_handlers/agent_codex"
-require "./ocawe/agent_function_handlers/agent_cliproxy"
-require "./ocawe/agent_function_handlers/agent_opencode"
-require "./ocawe/agent_function_handlers/agent_claude_code"
-require "./ocawe/agent_function_handlers/agent_qwen"
--- shards/agent-functions/src/ocawe/agent_function_handlers/helpers.cr
+++
@@ -1,5 +0,0 @@
-require "./helpers/provider_install"
-require "./helpers/provider_paths"
-require "./helpers/params_and_exec"
-require "./helpers/parsers"
-require "./helpers/federation_prompt"
